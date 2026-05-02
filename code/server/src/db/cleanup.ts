02e97b39f4c790f17893b74573e21793aa962739
gnnrsc <rosacegiovanna@gmail.com>
2026-05-02 14:43:46 +0200

    Initial commit: EZelectroncs

---
+++ code/server/src/db/cleanup.ts
@@ -0,0 +1,27 @@
+"use strict";
+
+import db from "../db/db";
+
+/**
+ * Deletes all data from the database.
+ * This function must be called before any integration test, to ensure a clean database state for each test run.
+ */
+
+export function cleanup() {
+  return new Promise<void>((resolve, reject) => {
+          db.run("DELETE FROM carts", (err) => {
+                  if (err) return reject(new Error("Cleanup error"));
+                  db.run("DELETE FROM reviews", (err) => {
+                      if (err) return reject(new Error("Cleanup error"));
+                      db.run("DELETE FROM products", (err) => {
+                          if (err) return reject(new Error("Cleanup error"));
+                          db.run("DELETE FROM users", (err) => {
+                              if (err) return reject(new Error("Cleanup error"));
+                              resolve();
+                          });
+                      });
+                  });
+              });
+          });
+}
+
